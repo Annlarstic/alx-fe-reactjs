cbde912efd0a206cf93f857156b71e75d58395ff
Annlarstic <nonsochi1823@gmail.com>
2025-03-01 18:59:07 +0100

    projects

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header'
+import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
@@ -8,9 +8,9 @@ function App() {
     return (
     <div>
       <WelcomeMessage />
-      <MainContent/>
-     <Header/>
-     <Footer/>
+       <Header />
+        <MainContent />
+        <Footer />
 
       </div>
     );
