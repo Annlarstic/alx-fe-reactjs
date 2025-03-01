7cf1769cb600df90c78eb182ab83247cde6c8f81
Annlarstic <nonsochi1823@gmail.com>
2025-03-01 19:08:28 +0100

    projects

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,10 @@
-const UserProfile = ({ name, age, bio }) => (
+const UserProfile = (props) => (
   <div>
-    <h2>{name}</h2>
-    <p>Age: {age}</p>
-    <p>Bio: {bio}</p>
+    <h2>{props.name}</h2>
+    <p>Age: {props.age}</p>
+    <p>Bio: {props.bio}</p>
   </div>
 );
 
 export default UserProfile;
+
