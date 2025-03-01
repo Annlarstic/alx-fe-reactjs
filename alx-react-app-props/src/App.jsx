5230b12c07bdd161af42619ba200cae95dbf6f35
Annlarstic <nonsochi1823@gmail.com>
2025-03-01 20:49:06 +0100

    Assignment

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Header from './components/Header';
-import WelcomeMessage from './components/WelcomeMessage';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import ProfilePage from './ProfilePage';  
-import UserContext from './UserContext';  
+import React from "react";
+import Header from "./components/Header";
+import WelcomeMessage from "./components/WelcomeMessage";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import ProfilePage from "./ProfilePage";
+import { UserProvider } from "./UserContext"; 
 
 function App() {
-    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
-    return (
-        <UserContext.Provider value={userData}>  
-            <div>
-                <Header />
-                <WelcomeMessage />
-                <MainContent />
-                <ProfilePage />  
-                <Footer />
-            </div>
-        </UserContext.Provider>
-    );
+  return (
+    <UserProvider>  
+      <div>
+        <Header />
+        <WelcomeMessage />
+        <MainContent />
+        <ProfilePage />  
+        <Footer />
+      </div>
+    </UserProvider>
+  );
 }
 
 export default App;
--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
-const UserContext = createContext(null);
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({ name: "Jane Doe", email: "jane.doe@example.com" });
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
 
 export default UserContext;
+
