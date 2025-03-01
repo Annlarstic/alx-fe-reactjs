f141b84dbd24bbaa6113ed9bf1438d9a507c5822
Annlarstic <nonsochi1823@gmail.com>
2025-03-01 19:27:30 +0100

    projects

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: "20px", backgroundColor: "#f4f4f4", minHeight: "300px" }}>
+      <p style={{ fontSize: "1.2rem" }}>Welcome to my favorite cities list!</p>
+    </main>
+  );
+}
+
+export default MainContent;
