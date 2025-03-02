ff189eea9f21ccd0a903379f258c6ec184a81514
Annlarstic <nonsochi1823@gmail.com>
2025-03-02 09:56:47 +0100

    Zustand Projects

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,21 @@
+import { useParams } from "react-router-dom";
+import useRecipeStore from "../recipeStore";
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === parseInt(id))
+  );
+
+  if (!recipe) return <p>Recipe not found!</p>;
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* Add edit and delete buttons here */}
+    </div>
+  );
+};
+
+export default RecipeDetails;
