8cded2d958e7a9cb92c9111b429e9d11096af79d
Annlarstic <nonsochi1823@gmail.com>
2025-03-09 14:09:43 +0100

    Registration Form

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import RegistrationForm from "./ Components/RegistrationForm";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 const App = () => {
   return (
     <div>
       <h1>User Registration</h1>
       <RegistrationForm />
+      <h2>Formik Form</h2>
+      <FormikForm />
     </div>
   );
 };
