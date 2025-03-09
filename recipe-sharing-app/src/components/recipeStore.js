29c8020af90063b4d13979f04ff96c3be5fd0998
Annlarstic <nonsochi1823@gmail.com>
2025-03-09 12:16:12 +0100

    Add Zustand Projects

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,8 +3,10 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
 
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
@@ -13,10 +15,14 @@ const useRecipeStore = create((set) => ({
       ),
     })),
 
+
   deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
     })),
+
+    
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
